53bd5dd061b91a0078097d298d894a39bbb1049a
Pavel Dikunets <dikunets.pavel@gmail.com>
2023-09-19 23:45:32 +0300

    Добавил модели для объявления

---
+++ src/AdvertisementsBoard/Contracts/AdvertisementsBoard.Contracts/Advertisements/AdvertisementShortInfoDto.cs
@@ -0,0 +1,22 @@
+namespace AdvertisementsBoard.Contracts.Advertisements;
+
+/// <summary>
+/// Модель объявления с краткой информацией.
+/// </summary>
+public class AdvertisementShortInfoDto
+{
+    /// <summary>
+    ///     Заголовок.
+    /// </summary>
+    public string Title { get; set; }
+    
+    /// <summary>
+    ///     Цена.
+    /// </summary>
+    public decimal Price { get; set; }
+
+    /// <summary>
+    ///     Наименование категории
+    /// </summary>
+    public string CategoryName { get; set; }
+}
