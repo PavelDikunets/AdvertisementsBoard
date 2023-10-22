f425183d022eba59b93cd9f91903fed4c23e79cc
Pavel Dikunets <dikunets.pavel@gmail.com>
2023-10-22 10:24:29 +0300

    Подправил конфигурацию сущностей

--- src/AdvertisementsBoard/Infrastructure/AdvertisementsBoard.Infrastructure.DataAccess/Contexts/Categories/Configurations/CategoryConfiguration.cs
+++ src/AdvertisementsBoard/Infrastructure/AdvertisementsBoard.Infrastructure.DataAccess/Contexts/Categories/Configurations/CategoryConfiguration.cs
@@ -14,10 +14,10 @@ public class CategoryConfiguration : IEntityTypeConfiguration<Category>
         builder.ToTable("Categories");
         builder.HasKey(p => p.Id);
         builder.Property(p => p.Id).ValueGeneratedOnAdd();
-        builder.Property(p => p.Name).IsRequired().HasMaxLength(100);
+        builder.Property(p => p.Name).HasMaxLength(100).IsRequired();
 
         builder.HasMany(p => p.SubCategories)
             .WithOne(b => b.Category)
-            .HasForeignKey(b => b.CategoryId).IsRequired();
+            .HasForeignKey(b => b.CategoryId);
     }
 }
--- src/AdvertisementsBoard/Infrastructure/AdvertisementsBoard.Infrastructure.DataAccess/Contexts/SubCategories/Configurations/SubCategoryConfiguration.cs
+++ src/AdvertisementsBoard/Infrastructure/AdvertisementsBoard.Infrastructure.DataAccess/Contexts/SubCategories/Configurations/SubCategoryConfiguration.cs
@@ -14,10 +14,10 @@ public class SubCategoryConfiguration : IEntityTypeConfiguration<SubCategory>
         builder.ToTable("SubCategories");
         builder.HasKey(p => p.Id);
         builder.Property(p => p.Id).ValueGeneratedOnAdd();
-        builder.Property(p => p.Name).IsRequired().HasMaxLength(100);
+        builder.Property(p => p.Name).HasMaxLength(100).IsRequired();
 
         builder.HasMany(p => p.Advertisements)
             .WithOne(b => b.SubCategory)
-            .HasForeignKey(b => b.SubCategoryId).IsRequired();
+            .HasForeignKey(b => b.SubCategoryId);
     }
 }
