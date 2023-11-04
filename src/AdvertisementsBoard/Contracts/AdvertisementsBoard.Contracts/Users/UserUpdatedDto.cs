8ed275706f37d0f582558b8ea0f1aec5de96bce0
Pavel Dikunets <dikunets.pavel@gmail.com>
2023-11-04 20:26:55 +0300

    Добавил модели Dto с обновленной информацией

---
+++ src/AdvertisementsBoard/Contracts/AdvertisementsBoard.Contracts/Users/UserUpdatedDto.cs
@@ -0,0 +1,12 @@
+namespace AdvertisementsBoard.Contracts.Users;
+
+/// <summary>
+///     Модель c обновленной информацией о пользователе.
+/// </summary>
+public class UserUpdatedDto
+{
+    /// <summary>
+    ///     Имя пользователя.
+    /// </summary>
+    public string Name { get; set; }
+}
