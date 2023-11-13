dede697035e87fe0fd4f3a32e9d4110e0d9b9cfe
Pavel Dikunets <dikunets.pavel@gmail.com>
2023-11-13 03:18:17 +0300

    Добавил реализацию получения всех объявлений пользователя, + рефакторинг

--- src/AdvertisementsBoard/Application/AdvertisementsBoard.Application.AppServices/Contexts/Advertisements/Repositories/IAdvertisementRepository.cs
+++ src/AdvertisementsBoard/Application/AdvertisementsBoard.Application.AppServices/Contexts/Advertisements/Repositories/IAdvertisementRepository.cs
@@ -14,7 +14,7 @@ public interface IAdvertisementRepository
     /// </summary>
     /// <param name="id">Идентификатор объявления.</param>
     /// <param name="cancellationToken">Токен отмены операции.</param>
-    /// <returns>Модель объявления <see cref="AdvertisementDto" />.</returns>
+    /// <returns>Модель объявления.</returns>
     Task<AdvertisementDto> GetByIdAsync(Guid id, CancellationToken cancellationToken);
 
     /// <summary>
@@ -23,24 +23,32 @@ public interface IAdvertisementRepository
     /// <param name="cancellationToken">Токен отмены операции.</param>
     /// <param name="pageNumber">Номер страницы.</param>
     /// <param name="pageSize">Размер страницы.</param>
-    /// <returns>Список объявлений с краткой информацией <see cref="AdvertisementShortInfoDto" />.</returns>
+    /// <returns>Список объявлений с краткой информацией.</returns>
     Task<List<AdvertisementShortInfoDto>> GetAllAsync(CancellationToken cancellationToken, int pageNumber,
         int pageSize);
 
     /// <summary>
+    ///     Получить все объявления пользователя.
+    /// </summary>
+    /// <param name="userId">Идентификатор пользователя.</param>
+    /// <param name="cancellationToken">Токен отмены операции.</param>
+    /// <returns>Список объявлений пользователя.</returns>
+    Task<List<AdvertisementShortInfoDto>> GetAllByUserIdAsync(Guid userId, CancellationToken cancellationToken);
+
+    /// <summary>
     ///     Создать объявление.
     /// </summary>
-    /// <param name="dto">Модель создания объявления <see cref="AdvertisementCreateDto" />.</param>
+    /// <param name="dto">Модель создания объявления.</param>
     /// <param name="cancellationToken">Токен отмены операции.</param>
     /// <returns>Идентификатор созданного объявления.</returns>
-    Task<Guid> CreateAsync(AdvertisementCreateDto dto, CancellationToken cancellationToken);
+    Task<Guid> CreateAsync(AdvertisementDto dto, CancellationToken cancellationToken);
 
     /// <summary>
     ///     Обновить объявление.
     /// </summary>
-    /// <param name="dto">Модель объявления <see cref="AdvertisementDto" />.</param>
+    /// <param name="dto">Модель объявления.</param>
     /// <param name="cancellationToken">Токен отмены операции.</param>
-    /// <returns>Модель с обновленным объявлением <see cref="AdvertisementUpdatedDto" />.</returns>
+    /// <returns>Модель с обновленным объявлением.</returns>
     Task<AdvertisementUpdatedDto> UpdateAsync(AdvertisementDto dto, CancellationToken cancellationToken);
 
     /// <summary>
@@ -51,20 +59,11 @@ public interface IAdvertisementRepository
     Task DeleteByIdAsync(Guid id, CancellationToken cancellationToken);
 
     /// <summary>
-    ///     Проверить, существует ли объявление с указанным фильтром.
-    /// </summary>
-    /// <param name="filter">Фильтр.</param>
-    /// <param name="cancellationToken">Токен отмены операции.</param>
-    /// <returns>Возвращает true, если объявление существует, и false в противном случае.</returns>
-    Task<bool> DoesAdvertisementExistWhereAsync(Expression<Func<Advertisement, bool>> filter,
-        CancellationToken cancellationToken);
-
-    /// <summary>
     ///     Получить объявление по фильтру.
     /// </summary>
     /// <param name="filter">Фильтр.</param>
     /// <param name="cancellationToken">Токен отмены операции.</param>
-    /// <returns>Модель объявления <see cref="AdvertisementDto" />.</returns>
-    Task<AdvertisementDto> GetWhereAsync(Expression<Func<Advertisement, bool>> filter,
+    /// <returns>Модель объявления.</returns>
+    Task<AdvertisementDto> FindWhereAsync(Expression<Func<Advertisement, bool>> filter,
         CancellationToken cancellationToken);
 }
--- src/AdvertisementsBoard/Application/AdvertisementsBoard.Application.AppServices/Contexts/Advertisements/Services/IAdvertisementService.cs
+++ src/AdvertisementsBoard/Application/AdvertisementsBoard.Application.AppServices/Contexts/Advertisements/Services/IAdvertisementService.cs
@@ -12,8 +12,16 @@ public interface IAdvertisementService
     /// </summary>
     /// <param name="id">Идентификатор объявления.</param>
     /// <param name="cancellationToken">Токен отмены операции.</param>
-    /// <returns>Модель с информацией об объявлении <see cref="AdvertisementInfoDto" />.</returns>
+    /// <returns>Модель с информацией об объявлении.</returns>
     Task<AdvertisementInfoDto> GetByIdAsync(Guid id, CancellationToken cancellationToken);
+
+    /// <summary>
+    ///     Получить список объявлений пользователя по его идентификатору.
+    /// </summary>
+    /// <param name="userId">Идентификатор пользователя.</param>
+    /// <param name="cancellationToken">Токен отмены операции.</param>
+    /// <returns>Модель с информацией об объявлениях пользователя.</returns>
+    Task<List<AdvertisementShortInfoDto>> GetAllByUserIdAsync(Guid userId, CancellationToken cancellationToken);
 
     /// <summary>
     ///     Получить постраничные объявления.
@@ -21,39 +29,43 @@ public interface IAdvertisementService
     /// <param name="cancellationToken">Токен отмены операции.</param>
     /// <param name="pageSize">Размер страницы.</param>
     /// <param name="pageNumber">Номер страницы.</param>
-    /// <returns>Список объявлений с краткой информацией <see cref="AdvertisementShortInfoDto" />.</returns>
+    /// <returns>Список объявлений с краткой информацией.</returns>
     Task<List<AdvertisementShortInfoDto>> GetAllAsync(CancellationToken cancellationToken, int pageSize,
         int pageNumber);
 
     /// <summary>
     ///     Создать объявление.
     /// </summary>
-    /// <param name="dto">Модель создания объявления <see cref="AdvertisementCreateDto" />.</param>
+    /// <param name="dto">Модель создания объявления.</param>
+    /// <param name="userId">Идентификатор пользователя.</param>
     /// <param name="cancellationToken">Токен отмены операции.</param>
     /// <returns>Идентификатор созданного объявления.</returns>
-    Task<Guid> CreateAsync(AdvertisementCreateDto dto, CancellationToken cancellationToken);
+    Task<Guid> CreateAsync(AdvertisementCreateDto dto, Guid userId, CancellationToken cancellationToken);
 
     /// <summary>
     ///     Обновить объявление по идентификатору.
     /// </summary>
     /// <param name="id">Идентификатор объявления</param>
-    /// <param name="dto">Модель обновления объявления <see cref="AdvertisementUpdateDto" />.</param>
+    /// <param name="userId"></param>
+    /// <param name="dto">Модель обновления объявления.</param>
     /// <param name="cancellationToken">Токен отмены операции.</param>
-    /// <returns>Модель с обновленным объявлением <see cref="AdvertisementUpdatedDto" />.</returns>
-    Task<AdvertisementUpdatedDto> UpdateByIdAsync(Guid id, AdvertisementUpdateDto dto,
+    /// <returns>Модель с обновленным объявлением.</returns>
+    Task<AdvertisementUpdatedDto> UpdateByIdAsync(Guid id, Guid userId, AdvertisementEditDto dto,
         CancellationToken cancellationToken);
 
     /// <summary>
     ///     Удалить объявление по идентификатору.
     /// </summary>
     /// <param name="id">Идентификатор объявления.</param>
+    /// <param name="userId">Идентификатор пользователя.</param>
     /// <param name="cancellationToken">Токен отмены операции.</param>
-    Task DeleteByIdAsync(Guid id, CancellationToken cancellationToken);
+    Task DeleteByIdAsync(Guid id, Guid userId, CancellationToken cancellationToken);
 
     /// <summary>
-    ///     Проверка объявления на существование по идентификатору.
+    ///     Получить идентификатор пользователя из объявления.
     /// </summary>
-    /// <param name="id">Идентификатор объявления.</param>
+    /// <param name="id">Идентификатор объявления</param>
     /// <param name="cancellationToken">Токен отмены операции.</param>
-    Task EnsureAdvertisementExistsByIdAsync(Guid id, CancellationToken cancellationToken);
+    /// <returns>Идентификатор пользователя.</returns>
+    Task<Guid> GetUserIdAsync(Guid id, CancellationToken cancellationToken);
 }
